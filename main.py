1582356193f8533ca6724cbad90f7f3e9bf7e98f
Inkognita <liepieshov@ucu.edu.ua>
2019-02-16 23:59:20 +0200

    ref

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ class FlyBooking:
                (self.id, self.client_name, self.fly_number,
                 self.from_loc, self.to_loc, self.date)
 
+
 class HotelBooking:
     def __init__(self, client_name, hotel_name, arrival, departure):
         self.id = None
@@ -32,6 +33,7 @@ class HotelBooking:
          VALUES (%s, %s, %s, %s, %s)""", \
                (self.id, self.client_name, self.hotel_name,
                 self.arrival, self.departure)
+
 
 class Account:
     def __init__(self, id_, client_name, amount):
@@ -48,42 +50,34 @@ class Account:
                (self.client_name, self.amount, self.id)
 
 
-try:
-    connection = {"db1": psycopg2.connect(user="postgres_user",
-                                          password="password",
-                                          host="127.0.0.1",
-                                          port="5432",
-                                          database="db1"),
-                  "db2": psycopg2.connect(user="postgres_user",
-                                          password="password",
-                                          host="127.0.0.1",
-                                          port="5432",
-                                          database="db2"),
-                  "db3": psycopg2.connect(user="postgres_user",
-                                          password="password",
-                                          host="127.0.0.1",
-                                          port="5432",
-                                          database="db3")}
-    cursor = {k: v.cursor() for k, v in connection.items()}
+if __name__ == "__main__":
+    try:
+        params = dict(user="postgres_user",
+                      password="password",
+                      host="127.0.0.1",
+                      port="5432")
+        connection = {"db1": psycopg2.connect(database="db3", **params),
+                      "db2": psycopg2.connect(database="db2", **params),
+                      "db3": psycopg2.connect(database="db3", **params)}
+        cursor = {k: v.cursor() for k, v in connection.items()}
 
+        ammount_to_pay = 600
+        acc = Account(id_=3, client_name="Bad man", amount=500)
+        acc.amount -= ammount_to_pay
+        fly = FlyBooking(client_name=acc.client_name, fly_number="FLY123", from_loc="LCA", to_loc="UKR",
+                         date=time.time())
 
-    ammount_to_pay = 600
-    acc = Account(id_=3, client_name="Bad man", amount=500)
-    acc.amount -= ammount_to_pay
-    fly = FlyBooking(client_name=acc.client_name, fly_number="FLY123", from_loc="LCA", to_loc="UKR", date=time.time())
+        cursor["db3"].execute(*acc.update())
+        cursor["db1"].execute(*fly.create())
 
-    cursor["db3"].execute(*acc.update())
-    cursor["db1"].execute(*fly.create())
-
-    connection["db1"].commit()
-    connection["db3"].commit()
-except psycopg2.DatabaseError as error :
-    print ("Error in transction Reverting all other operations of a transction ", error)
-    connection["db3"].rollback()
-    connection["db1"].rollback()
-    connection["db2"].rollback()
-finally:
-    for name, con in connection.items():
-        if con:
-            cursor[name].close()
-            con.close()
+        connection["db1"].commit()
+        connection["db3"].commit()
+    except psycopg2.DatabaseError as error:
+        print("Error in transction Reverting all other operations of a transction ", error)
+        for con in connection.values():
+            con.rollback()
+    finally:
+        for name, con in connection.items():
+            if con:
+                cursor[name].close()
+                con.close()
